92cd19faba02eef71ada1fbbe01f7adff56cda7a
JiaoJiao123 <jiaojiaowanghere@gmail.com>
2017-12-11 17:28:22 -0500

    match miner curation interface

---
+++ src/main/webapp/app/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: 'AIzaSyBstX73oT9UftO3hc28cQi94yhrhXMl8lY',
+        authDomain: 'clinical-trials-393f1.firebaseapp.com',
+        databaseURL: 'https://clinical-trials-393f1.firebaseio.com',
+        projectId: 'clinical-trials-393f1',
+        storageBucket: 'clinical-trials-393f1.appspot.com',
+        messagingSenderId: '179925618462'
+    }
+};
