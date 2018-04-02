7407f2a88dc6512ae3bfde10e5b6f988465a0e49
jingsu <victoriasu1994@gmail.com>
2018-04-01 21:59:29 -0400

    Delete matchminer-engine directoy before git clone

--- src/main/java/org/mskcc/oncokb/config/MatchEngineConfiguration.java
+++ src/main/java/org/mskcc/oncokb/config/MatchEngineConfiguration.java
@@ -1,9 +1,13 @@
 package org.mskcc.oncokb.config;
 
+import org.apache.commons.io.FileUtils;
 import org.mskcc.oncokb.service.util.PythonUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import javax.annotation.PostConstruct;
+import java.nio.file.Files;
+import java.io.File;
+import java.nio.file.Paths;
 
 @Configuration
 public class MatchEngineConfiguration {
@@ -18,7 +22,11 @@ public class MatchEngineConfiguration {
     @PostConstruct
     public void cloneMatchEngineFromGit() {
         try {
-            ProcessBuilder pbClone = new ProcessBuilder("git", "clone", this.gitUrl, "-b", this.branch);
+            if (Files.exists(Paths.get(this.matchengineAbsolutePath))) {
+                FileUtils.cleanDirectory(new File(this.matchengineAbsolutePath));
+            }
+            ProcessBuilder pbClone = new ProcessBuilder("git", "clone", this.gitUrl, "-b", this.branch,
+                this.matchengineAbsolutePath);
             Boolean isClone = PythonUtil.runPythonScript(pbClone);
 
             if(isClone){
