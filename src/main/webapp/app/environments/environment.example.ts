54966c7809b761972ea6c5819b4624ee974db692
jingsu <victoriasu1994@gmail.com>
2018-04-12 10:39:14 -0400

    Recover environment.example.ts

---
+++ src/main/webapp/app/environments/environment.example.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: '',
+        authDomain: '',
+        databaseURL: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: ''
+    },
+    oncokb: true, // set to true to customized interface for oncokb
+    production: false // set to false if doing frontend development
+};
